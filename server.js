ab03f481263481d5c830a6ce2f5f04dc2491b782
kliff9 <c.thelusca@hotmail.com>
2022-10-15 06:48:58 -0400

    update

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import userRouter from './Routers/UserRouter.js';
-import productRouter from './Routers/ProductRouter.js';
-import dotenv from 'dotenv';
-import orderRouter from './Routers/OrderRoute.js';
-import path from 'path'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./Routers/UserRouter.js";
+import productRouter from "./Routers/ProductRouter.js";
+import dotenv from "dotenv";
+import orderRouter from "./Routers/OrderRoute.js";
+import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
@@ -19,36 +19,31 @@ app.use(express.urlencoded({ extended: true }));
 //   useUnifiedTopology: true,
 // });
 
-mongoose.connect("mongodb+srv://kliff01:kliff01@e-com.qk1or.mongodb.net/E-comDB?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+mongoose.connect(
+  "mongodb+srv://kliff01:kliff01@e-com.qk1or.mongodb.net/E-comDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(cors());
 
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
+app.get("/run", (req, res) => {
+  res.send("Server is ready");
+});
 
-app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-});  
-
-
-
-app.get('/run', (req, res) => {
-    res.send('Server is ready');
-  });
-
-
-  app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
-  });
-  
-
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
@@ -57,18 +52,26 @@ app.listen(port, () => {
 });
 const __dirname = path.resolve();
 
+//   if (process.env.NODE_ENV === "production") {
+//  app.use(express.static(path.join(__dirname, "/frontend/build")));
+//  app.get('*', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+//  })
+//  } else {
+//   app.get('/', (req, res) => {
+//     res.send('Server is ready');
+//   });
+//  }
 
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-  if (process.env.NODE_ENV === "production") {
- app.use(express.static(path.join(__dirname, "/frontend/build")));
- app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
- }) 
- } else {
-  app.get('/', (req, res) => {
-    res.send('Server is ready');
-  });
- }
-
-
-
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
