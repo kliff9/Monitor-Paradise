5cb79dbde3e5248c1eebbe39d72710378e522f25
kliff9 <c.thelusca@hotmail.com>
2022-10-15 20:24:15 -0400

    update

--- server.js
+++ server.js
@@ -63,11 +63,16 @@ const __dirname = path.resolve();
 //   });
 //  }
 
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./frontend/build/index.html"),
+    path.join(__dirname, "frontend", "build", "index.html"),
+
     function (err) {
       if (err) {
         res.status(500).send(err);
