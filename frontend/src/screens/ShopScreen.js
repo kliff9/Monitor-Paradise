5cba214e773d543e9e70a5ddd0f67d29f5e85f77
kliff9 <c.thelusca@hotmail.com>
2022-10-15 22:30:03 -0400

    update

--- frontend/src/screens/ShopScreen.js
+++ frontend/src/screens/ShopScreen.js
@@ -1,38 +1,33 @@
-import {useEffect} from "react"
-import Product from "../componets/Product"
+import { useEffect } from "react";
+import Product from "../componets/Product";
 import LoadingBox from "../componets/LoadingBox";
 import MessageBox from "../componets/MessageBox";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 export default function ShopScreen() {
+  const productList = useSelector((state) => state.productList);
+  const { loading, products, error } = productList;
+  const dispatch = useDispatch();
 
-const productList = useSelector( state => state.productList) 
-const { loading, products, error } = productList
-const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(listProducts());
+  }, []);
 
-
-useEffect(() => { 
-  dispatch(listProducts())
-}, [])
-  
- 
-    return (
-
-        <div>
-          
-          {loading? <LoadingBox></LoadingBox> : error? <MessageBox variant="danger">{error}</MessageBox> : (
-         <div className="row center">         
-          {products.map((product) => (            
-           <Product key={product._id} product={product}> </Product>
-          ))};
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product}>
+              {" "}
+            </Product>
+          ))}
         </div>
-          
-)}
-
-
-      </div>
-      
-
-    )
-    
+      )}
+    </div>
+  );
 }
